77aad69706f63fc1bdd4525c911414e50414ccae
ZackRogers <rogerszackery1@gmail.com>
2021-04-03 09:39:03 -0400

    data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,22 @@
-// from data.js
-var tableData = data;
+show(data);
 
-// YOUR CODE HERE!
+function show(data) {
+    d3.select('tbody').html('');
+
+    data.forEach(obj => {
+        var row = d3.select('tbody').append('tr');
+        Object.values(obj).forEach(val=>{
+            row.append('td').text(val)
+        })
+    });
+};
+
+d3.select('button').on('click',handleClick);
+
+function handleClick() {
+    var val = d3.select('input').property('value');
+
+    var filteredData = data.filter(obj=>obj.datetime == val);
+    show(filteredData);
+};
+
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,32 @@
-// from data.js
-var tableData = data;
+show(data);
 
-// YOUR CODE HERE!
+function show(data) {
+    d3.select('tbody').html('');
+    
+    data.forEach(obj => {
+        var row = d3.select('tbody').append('tr');
+        Object.values(obj).forEach(val=>{
+            row.append('td').text(val)
+        })
+    });
+
+    d3.select('button').on('click',handleClick);
+    d3.selectAll('input').on('change',handleChange);
+};
+
+var filteredData  = data;
+
+function handleClick() {
+    d3.selectAll('input').property('value','');
+    filteredData = data;
+    show(filteredData);
+};
+
+function handleChange() {
+    var key = d3.select(this).property('id');
+    var val = d3.select(this).property('value');
+
+    filteredData = filteredData.filter(obj=>obj[key] == val);
+    show(filteredData);
+}
+
